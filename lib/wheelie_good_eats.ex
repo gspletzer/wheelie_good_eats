0763e7853da18624d95fef6126b76082739e9ef1
Grace Spletzer <gspletzer@influxdata.com>
2023-05-26 19:17:34 -0700

    feat: add initial functionality to top-level api layer

--- lib/wheelie_good_eats.ex
+++ lib/wheelie_good_eats.ex
@@ -1,9 +1,32 @@
 defmodule WheelieGoodEats do
   @moduledoc """
-  WheelieGoodEats keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  Top-level API for the Wheelie Good Eats application.
   """
+
+  alias WheelieGoodEats.Truck
+
+  @doc """
+  Fetches all food trucks for the client.
+  """
+  @spec show_all :: [Truck.t()]
+  def show_all() do
+    Truck.fetch_all_trucks()
+  end
+
+  @doc """
+  Fetches all food trucks that match query filter
+  provided by client.
+  """
+  @spec show_query(String.t()) :: [Truck.t()]
+  def show_query(filter) do
+    Truck.fetch_query_trucks(filter)
+  end
+
+  @doc """
+  Fetches a random food truck for the client.
+  """
+  @spec show_random :: [Truck.t()]
+  def show_random() do
+    Truck.fetch_random_truck()
+  end
 end
