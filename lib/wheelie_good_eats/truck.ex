bc5ebd238865f194143db91fa4f3edfb26987088
Grace Spletzer <gspletzer@influxdata.com>
2023-05-26 18:39:54 -0700

    chore: add queries to Truck module

--- lib/wheelie_good_eats/truck.ex
+++ lib/wheelie_good_eats/truck.ex
@@ -1,6 +1,10 @@
 defmodule WheelieGoodEats.Truck do
   use Ecto.Schema
 
+  alias WheelieGoodEats.Repo
+
+  import Ecto.Query, only: [from: 2]
+
   schema "trucks" do
     field :applicant, :string
     field :address, :string
@@ -8,5 +12,24 @@ defmodule WheelieGoodEats.Truck do
     field :food_items, :string
     field :schedule, :string
     timestamps()
+  end
+
+  def fetch_all_trucks() do
+    Repo.all(WheelieGoodEats.Truck)
+  end
+
+  def fetch_query_trucks(filter) do
+    included = "%#{filter}%"
+
+    from(t in WheelieGoodEats.Truck,
+      select: t,
+      where: ilike(t.food_items, ^included)
+    )
+    |> Repo.all()
+  end
+
+  def fetch_random_truck() do
+    Repo.all(WheelieGoodEats.Truck)
+    |> Enum.random()
   end
 end
